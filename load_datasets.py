7c73095b3fe5f8ac8b57ab21124cf7e82ad363b7
doppioanalytics <jazbaygrps@protonmail.com>
2026-07-13 17:26:54 -0300

    Updated script

--- load_datasets.py
+++ load_datasets.py
@@ -7,7 +7,7 @@ path = "C:\\Users\\Gustavo\\Desktop\\case_echoenergia\\datasets"
 csv_files = glob.glob(os.path.join(path, "*.csv"))
 
 # Inserir todos os dfs em uma lista
-df = [pd.read_csv(f) for f in csv_files]
+df = [pd.read_csv(f, sep=';') for f in csv_files]
 
 # Concatenar todos os dataframes em um consolidado
 df_consolidado = pd.concat(df)
@@ -18,6 +18,4 @@ df_consolidado.to_parquet('df_consolidado.parquet', engine='pyarrow')
 df_parquet = pd.read_parquet('df_consolidado.parquet')
 
 print(df_parquet.head())
-
-
 
